eb77a0782430c555b8e37446836d13018a481940
bennyboy23 <cricketgenius.evans@gmail.com>
2016-06-29 16:14:46 +0100

    Comments

---
+++ WebApplication2/Models/Comments.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Comments
+    {
+        public int CommentsID { get; set; }
+
+        public string Comment { get; set; }
+
+    }
+}
